dbe6a806b54c503b1a0909d629c4135517b642e5
Domenico Amato <amatonico2001@gmail.com>
2023-05-23 19:57:19 +0200

    implentati lavori con css riguardanti annunci - aggiunti circlenatura,relax,history e culture con relative immagini rounded-circle

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,4 +1,32 @@
 <x-layout header="Stai visualizzando {{$category->name}}">
+
+   <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="circle-nature circle"><img class="rounded-circle" alt="avatar1" src="/media/nature.svg" /></div>
+        <div><h4>Natura e avventura</h4></div>
+        <strong class="color-nature">100%</strong>
+      </div>
+      <div class="col">
+        <div class="circle-relax circle"><img class="rounded-circle" alt="avatar1" src="/media/relax.svg"/></div>
+        <div class="ps-4"><h4>Relax e mare</h4></div>
+        <strong class="ps-4 color-relax">80%</strong>
+      </div>
+      <div class="col">
+        <div class="circle-history circle"><img class="rounded-circle" alt="avatar1" src="/media/history.svg" /></div>
+        <div><h4>Monumenti e storia</h4></div>
+        <strong class="color-history">20%</strong>
+      </div>
+      <div class="col">
+        <div class="circle-culture circle"><img class="rounded-circle" alt="avatar1" src="/media/culture.svg" /></div>
+        <div class="ps-4"><h4>Città e cultura</h4></div>
+        <strong class="ps-4 color-culture">60%</strong>
+      </div>
+    </div>
+  </div>
+
+
+
     <div class="min-vh-100">
         <div class="container-fluid my-5">
                     <div class="row justify-content-center">
@@ -6,7 +34,7 @@
                             @forelse ($category->articles as $article)
                                 <div class="col-12 col-md-4 p-3">
     
-                                    <div class="card">
+                                    <div class="card bg-transparent">
                                         <img src="https://picsum.photos/300" class="card-img-top p-3 rounded" alt="immagine">
                                         <div class="card-body">
                                         <h5 class="card-title">{{$article->name}}</h5>
@@ -17,6 +45,8 @@
                                         </div>
                                     </div>
                                 </div>
+
+
                             @empty
     
                             <div class="col-12 text-center">

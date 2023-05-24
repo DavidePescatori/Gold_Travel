81fd750b14b306331a66c2f1909959584c5649d2
PaoloPetrassi <128042245+PaoloPetrassi@users.noreply.github.com>
2023-05-24 10:19:25 +0200

    modifiche

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,10 +26,7 @@
                     </ul>
                 </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success color-plumViolet" type="submit">Search</button>
-        </form>
+       
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
                

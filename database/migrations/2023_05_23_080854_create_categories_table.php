1db1ed125f7dc144b0dfd35fd941fb04c82dfc4d
Davide Pescatori <pescatoridavide7@gmail.com>
2023-05-26 13:14:15 +0200

    aggiornati numeri percentuale dentro db seeder

--- database/migrations/2023_05_23_080854_create_categories_table.php
+++ database/migrations/2023_05_23_080854_create_categories_table.php
@@ -39,7 +39,7 @@ return new class extends Migration
             "Toscana",
             "Trentino Alto Adige",
             "Umbria",
-            "Valle d'Aosta",
+            "Valle d\'Aosta",
             "Veneto",
         ];
 
